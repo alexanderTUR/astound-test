939887ad39071534d4d75f8dbcbda9c36bef0b6e
Александр Тур <alexandertur.fe@gmail.com>
2021-03-17 17:01:19 +0200

    added product section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-// Place your JavaScript code here.
+// Open/close social share block
+const socialShareOpenButton = document.querySelector('.social-share-widget__open-button');
+const socialShareCloseButton = document.querySelector('.social-share-widget__close-button');
+const socialShareWidgetBlock = document.querySelector('.social-share-widget__block');
+
+if (socialShareOpenButton) {
+  socialShareOpenButton.addEventListener('click', () => {
+    socialShareWidgetBlock.classList.add('social-share-widget__block_opened');
+  });
+}
+
+if (socialShareCloseButton) {
+  socialShareCloseButton.addEventListener('click', () => {
+    socialShareWidgetBlock.classList.remove('social-share-widget__block_opened');
+  });
+}
